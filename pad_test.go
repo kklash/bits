8fc855c84b8c580448d6713e07d76ecdc198f5e5
kklash <konnor@exodus.io>
2022-02-17 15:01:39 -0800

    tests: move SplitPadLeft test to split_test.go

--- pad_test.go
+++ pad_test.go
@@ -25,31 +25,3 @@ func TestBits_PadLeft(t *testing.T) {
 	test(2, "1000001", "01000001")
 	test(5, "", "00000")
 }
-
-func TestSplitPadLeft(t *testing.T) {
-	test := func(n uint32, bitString string, expectedBitGroups ...string) {
-		bits, _ := StringToBits(bitString)
-		if bits == nil {
-			t.Errorf("Failed to parse bits from string: %s", bitString)
-			return
-		}
-
-		bitGroups := bits.SplitPadLeft(n)
-		if len(bitGroups) != len(expectedBitGroups) {
-			t.Errorf("Split bit groups length does not match\nWanted %d\nGot    %d", len(expectedBitGroups), len(bitGroups))
-			return
-		}
-
-		for i := 0; i < len(bitGroups); i++ {
-			if group := bitGroups[i].String(); group != expectedBitGroups[i] {
-				t.Errorf("Split bit group does not match\nWanted %s\nGot    %s", expectedBitGroups[i], group)
-			}
-		}
-	}
-
-	test(8, "101010101111111100000000", "10101010", "11111111", "00000000")
-	test(8, "111100001111", "00001111", "00001111")
-	test(11, "101010101010101010", "00001010101", "01010101010")
-	test(1, "00001111", "0", "0", "0", "0", "1", "1", "1", "1")
-	test(0, "00001111")
-}
